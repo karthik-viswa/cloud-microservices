f9689f4ef7628c6b0af047f3db701dd6d36fcbf7
Karthik <karthik.viswa@gmail.com>
2019-03-16 19:03:49 +0000

    limits service configured by spring cloud service

---
+++ code/limits-service/src/main/java/com/karthik/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
